1ab03d7c6357029f27df1a68f3b85b28f098e343
Gabor Horvath-Ulenius <ghorvath@student.hive.fi>
2021-12-10 11:39:22 +0200

    merged

--- srcs/structure/sandbox.c
+++ srcs/structure/sandbox.c
@@ -6,11 +6,7 @@
 /*   By: ghorvath <ghorvath@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 13:42:39 by briffard          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/12/10 09:18:54 by ghorvath         ###   ########.fr       */
-=======
 /*   Updated: 2021/12/10 09:35:05 by briffard         ###   ########.fr       */
->>>>>>> 2c7b7d182d4393682f84048960b32cad9c696220
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +15,6 @@
 
 static char	*file_name(char *fn)
 {
-<<<<<<< HEAD
-
-	int	lvl = 0;
-	int	ret;
-
-/*----------------------- INTRODUCTION TEXTE -------------------------------*/
-
-	system("clear");
-	void	sandbox_header();
-
-	printf("Welcome to the sandbox session!\n");
-	printf("The sandbox mod offer you the possibility to pratice your coding skill without any contraint!\n");
-	printf("It' s only about the learning!\n");
-
-	printf("Fisrt, you have to choose your starting level. There are 5 levels.\n");
-	printf("Level 0 is the easiest and the 5th is the most difficiult one\n");
-=======
 	fn[ft_strlen(fn)] = '\0';
 	return (fn);
 }
@@ -52,24 +31,10 @@ int	main()
 		ft_putstrcolor("ERROR WELCOME MESSAGE", "red");
 	ft_display_file(fd);
 	close(fd);
->>>>>>> 2c7b7d182d4393682f84048960b32cad9c696220
 
 /*--------------------------- CHOICE OF LEVEL -------------------------------*/
 
 	printf("At witch level do you want start ? \n");
-<<<<<<< HEAD
-
-	//ret = scanf("%d", &lvl);
-
-	if (ret > 0 && ret < 5)
-		ret = scanf("%d", &lvl);
-	else
-		perror("ERROR! Invalid character!");
-
-	/*if (!ret) in case of problem
-		printf("ERROR");*/
-// we should also handle if the use choice is n > 0 ou n < 5
-=======
 	ret = scanf("%d", &lvl);
 	if (!ret) //in case of problem
 		printf("ERROR");
@@ -95,7 +60,6 @@ int	main()
 	 * */
 
 	openinstruction(file_name(test));
->>>>>>> 2c7b7d182d4393682f84048960b32cad9c696220
 
 
 }

1991c243fd46e4e647bf70d473ca9d323045b576
root <root@Chopper.localdomain>
2021-12-18 16:04:01 +0200

    MAJ sandbox.c with different ideas

--- srcs/structure/sandbox.c
+++ srcs/structure/sandbox.c
@@ -6,12 +6,12 @@
 /*   By: ghorvath <ghorvath@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 11:39:54 by briffard          #+#    #+#             */
-/*   Updated: 2021/12/10 13:39:07 by briffard         ###   ########.fr       */
+/*   Updated: 2021/12/18 16:02:39 by briffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"./include/libexam.h"
-#include "./libft/includes/libft.h"
+//#include "./libft/includes/libft.h"
 
 static char	*file_name(char *fn)
 {
@@ -21,10 +21,11 @@ static char	*file_name(char *fn)
 
 int	main()
 {
-	int		fd, fd1, lvl, ret;
+	int		fd, lvl, ret;
+    int secondchoice;
 	char	*test;
-	fd1 = 0;
 
+    system("clear");
 	sandbox_header();
 
 /*----------------------- INTRODUCTION TEXTE --------------------------------*/
@@ -44,7 +45,9 @@ int	main()
   *  we should also handle if the user choice is n > 0 ou n < 5
  */
 /*---------------------GENERATOR FUNCTION -----------------------------------*/
-		test = function_generator(lvl);
+
+    test = function_generator(lvl);
+
 /*--------------------DISPLAY INSTRUCTION------------------------------------*/
 
 	/*CREATION OF THE SUMISSION FOLDER*/
@@ -58,9 +61,63 @@ int	main()
 	 * we can try also to copy test in a new str chr by chr and stop before the
 	 * \n
 	 * */
-	sandbox_header();
+
+    sandbox_header();
 	openinstruction(file_name(test));
 
+/*------------------------------ FILE CHECKER -------------------------------------*/
+
+/*Differents ideas to check file
+-> check forbiden function (bash script take a look at the libfttester
+-> Check normi : thhis can be activate or desactivate)! if norminette is not present we should install it 
+-> check compilation : we are looking for compilation problem
+-> check memory leaks : maybe more for a hard core mode
+-> check the output : still think about it : some checker use strcmp 
+-> 
+*/
+
+/*correction*/
+/* We should offer the possibility in case of failure to explain what is expected and how to get the output
+we can do some functions who will explain line by line with the evolution of the variable 
+It can take differents form . Something to read, or it can also in an interactive way like:
+EX:
+write(1,_____,1);
+
+What should be there instead of _______
+your answer:
+
+It can take multiple answer like
+EX:
+
+void ft_putstr(1._______  str)
+{
+    if(!2.___)
+        3.____ (NULL);
+    4.____
+        while(5.______)
+            ft_putchar(6.____);
+}
+Please Write the answers 
+1.
+2.
+3.
+...
+
+we can can create function HELP who give you acces to definition of function of diiferent 
+
+We can maybe also create some multiple choice answer test.
+*/
+
+/*----------------------------------------------------------------------------------*/
+    ft_putchar('\n');
+    printf("-----------------------------------------------------------------\n");                
+    ft_putstrcolor("1 - NEXT FUNCTION ","green");
+    ft_putstrcolor(" 2 - LVL DOWN ", "blue");
+    ft_putstrcolor(" 3 - LVL UP ", "red");
+    ft_putstrcolor(" 4 - SUBMIT ", "yellow");
+    ft_putstrcolor(" 5 - EXIT \n","white");
+    printf("-----------------------------------------------------------------\n");
+    scanf("%d", &secondchoice);
 
 }
 

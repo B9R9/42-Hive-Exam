df8137c230c83a1d437f913f112e933aea3d40a6
Baptiste Riffard <briffard@c2r1p5.hive.fi>
2021-12-23 13:25:34 +0200

    UPDATE CREATED DO WHILE LOOP IN SAND BOXWITH INCRENATATION OF LVL // ADD INSTRUCTION SUBJECT IN OPENINSTRUCTION

--- srcs/structure/sandbox.c
+++ srcs/structure/sandbox.c
@@ -6,7 +6,7 @@
 /*   By: ghorvath <ghorvath@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 11:39:54 by briffard          #+#    #+#             */
-/*   Updated: 2021/12/10 16:00:53 by briffard         ###   ########.fr       */
+/*   Updated: 2021/12/23 12:48:24 by briffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,10 @@ int	main()
   *  we should also handle if the user choice is n > 0 ou n < 5
  */
 /*---------------------GENERATOR FUNCTION -----------------------------------*/
-		test = function_generator(lvl);
+	do
+	{	
+	test = function_generator(lvl);
+
 /*--------------------DISPLAY INSTRUCTION------------------------------------*/
 
 	/*CREATION OF THE SUMISSION FOLDER*/
@@ -61,8 +64,9 @@ int	main()
 	 * \n
 	 * */
 	sandbox_header();
-	openinstruction(file_name(test));
-	
+	printf("CURRENT  LVL : %d\n", lvl);//find a nicer place 
+	openinstruction(file_name(test), lvl);
+/*---------------------- MENU END -------------------------------------------*/	
 	ft_putchar('\n');
 	printf("-----------------------------------------------------------------\n");
 	ft_putstrcolor("1 - NEXT FUNCTION ","green");
@@ -72,7 +76,25 @@ int	main()
 	ft_putstrcolor(" 5 - EXIT \n","white");
 	printf("-----------------------------------------------------------------\n");
 	scanf("%d", &secondchoice);
-	
 
+	switch(secondchoice)
+	{
+		case 1:
+			test = function_generator(lvl);
+			break ;
+		case 2:
+			if (lvl > 0)
+				lvl -= 1;
+			test = function_generator(lvl);
+			break ;
+		case 3:
+			if (lvl < 5)
+				lvl += 1;
+			test = function_generator(lvl);
+			break ;
+		case 4:
+			/*NEED CHECKER CODE */
+			break ;
+	}
+	}while (secondchoice != 5);
 }
-
